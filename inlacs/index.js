bb0e46fe1451c315e43ba01b62e78958e2fd1568
malaydhamelia <malaydhameliasoftwares@gmail.com>
2020-09-09 18:23:21 +0530

    lkjlj

---
+++ inlacs/index.js
@@ -0,0 +1,14 @@
+var image = document.getElementById("news"), 
+    button = document.getElementById("pause");
+    
+if (image.classList && image && button) {
+    button.onclick = function() {
+        if (this.value == 'pause') {
+            image.classList.add('pause');
+            this.value = 'play';
+        } else {
+            image.classList.remove('pause');
+            this.value = 'pause';
+        }
+    };
+}
--- temp/coordinates.pde
+++
@@ -1,9 +0,0 @@
-/* @pjs preload=""technical.png; */
-PImage fullimage;
-
-void setup(){
-  size(1366, 768);
-  fullimage = loadImage("technical.png");
-  image(fullimage, 0,0);
-  
-}
